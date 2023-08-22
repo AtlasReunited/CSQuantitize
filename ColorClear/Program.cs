bb55aa92f4b8da6c249ed41a4b9c088a54e7f26f
Atlas <thegalaxy2015@yahoo.com>
2023-08-21 20:35:46 -0700

    Initial Version

---
+++ ColorClear/Program.cs
@@ -0,0 +1,34 @@
+﻿using ImageMagick;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a image path: ");
+        string inputImagePath = @Console.ReadLine() ?? throw new InvalidOperationException();
+        Console.WriteLine("How many colors do you want?");
+        int desiredMaxColors = Convert.ToInt32(Console.ReadLine());
+        string outputImagePath = @"C:\Users\thega\Downloads\output.png";
+        try
+        {
+            using (MagickImage inputImage = new MagickImage(inputImagePath))
+            {
+                ProcessAndSaveQuantizedImage(inputImage, desiredMaxColors, outputImagePath);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+    static void ProcessAndSaveQuantizedImage(MagickImage image, int maxColors, string outputPath)
+    {
+        image.Quantize(new QuantizeSettings
+        {
+            Colors = maxColors
+        });
+        image.Write(outputPath);
+        Console.WriteLine($"Color quantization with {maxColors} colors complete.");
+    }
+    
+    
+}
