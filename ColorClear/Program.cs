57ed53f11c2255e0ab7b725014f5ef67e439d8ad
Atlas <thegalaxy2015@yahoo.com>
2023-08-21 23:32:19 -0700

    More checks in code. A bit messy, but I'll update later

--- ColorClear/Program.cs
+++ ColorClear/Program.cs
@@ -5,17 +5,35 @@ class Program
     {
         Console.WriteLine("Enter a image path: ");
         string inputImagePath = @Console.ReadLine(); //If file doesn't exist or is null, ask for a valid file path
+        //Check the file paths quickly just to be sure there weren't accidental ""s surrounding the path
+        if(inputImagePath[0].Equals('"') && inputImagePath[^1].Equals('\"'))
+        {
+            inputImagePath = inputImagePath.Substring(1, inputImagePath.Length - 2);
+        }
         while (!File.Exists(inputImagePath) || inputImagePath == null)
         {
             Console.WriteLine("File does not exist. Please enter a valid file path: ");
             inputImagePath = @Console.ReadLine();
+            if(inputImagePath[0].Equals('"') && inputImagePath[^1].Equals('\"'))
+            {
+                inputImagePath = inputImagePath.Substring(1, inputImagePath.Length - 2);
+            }
         }
-        Console.Write("Where do you want to save the output location?");
+        Console.WriteLine("Where do you want to save the output location?");
         string outputImagePath = @Console.ReadLine();
+        if (outputImagePath[0].Equals('\"') && outputImagePath[^1].Equals('\"'))
+        {
+            outputImagePath = outputImagePath.Substring(1, outputImagePath.Length - 2);
+        }
+        
         while (outputImagePath == null)
         {
             Console.WriteLine("Please enter a valid file path: ");
             outputImagePath = @Console.ReadLine();
+            if (outputImagePath[0].Equals('\"') && outputImagePath[^1].Equals('\"'))
+            {
+                outputImagePath = outputImagePath.Substring(1, outputImagePath.Length - 2);
+            }
         }
         //Double check to see if it includes the file name + extension, or if it's just the folder path. If no file is provided, append "output.png" to the end of the path.
         if (!outputImagePath.Contains("."))
@@ -35,6 +53,8 @@ class Program
         {
             Console.WriteLine("An error occurred: " + ex.Message);
         }
+        Console.ReadLine();
+
     }
     static void ProcessAndSaveQuantizedImage(MagickImage image, int maxColors, string outputPath)
     {
