d124f4d20d085f8e6a1372b995110b0100103ff8
Atlas <thegalaxy2015@yahoo.com>
2023-08-21 23:15:29 -0700

    Fully CLI-runnable. Ready for building.

--- ColorClear/Program.cs
+++ ColorClear/Program.cs
@@ -4,10 +4,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a image path: ");
-        string inputImagePath = @Console.ReadLine() ?? throw new InvalidOperationException();
+        string inputImagePath = @Console.ReadLine(); //If file doesn't exist or is null, ask for a valid file path
+        while (!File.Exists(inputImagePath) || inputImagePath == null)
+        {
+            Console.WriteLine("File does not exist. Please enter a valid file path: ");
+            inputImagePath = @Console.ReadLine();
+        }
+        Console.Write("Where do you want to save the output location?");
+        string outputImagePath = @Console.ReadLine();
+        while (outputImagePath == null)
+        {
+            Console.WriteLine("Please enter a valid file path: ");
+            outputImagePath = @Console.ReadLine();
+        }
+        //Double check to see if it includes the file name + extension, or if it's just the folder path. If no file is provided, append "output.png" to the end of the path.
+        if (!outputImagePath.Contains("."))
+        {
+            outputImagePath += "output.png";
+        }
         Console.WriteLine("How many colors do you want?");
         int desiredMaxColors = Convert.ToInt32(Console.ReadLine());
-        string outputImagePath = @"C:\Users\thega\Downloads\output.png";
         try
         {
             using (MagickImage inputImage = new MagickImage(inputImagePath))
